be6fad3dba062d8296efd30a8559ce1da1b271c0
async-devil <69465334+async-devil@users.noreply.github.com>
2020-10-25 22:20:02 +0200

    Created basic contributions parser

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-// TODO: create parse method
+const request = require('request');
+const cheerio = require('cheerio');
+
+const user = 'async-devil';
+const url = `https://github.com/users/${user}/contributions`;
+let contributions;
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const $ = cheerio.load(body);
+    contributions = $('.js-calendar-graph-svg').html();
+  } else {
+    throw new Error(error);
+  }
+});
+
+export { contributions as default };

18d15dd3a6d69f3919e4b26ca4bbe74cdf704091
async-devil <async.devil@gmail.com>
2020-11-12 00:23:17 +0200

    Added some RegEx

--- src/main.js
+++ src/main.js
@@ -9,7 +9,14 @@ request(url, (error, response, body) => {
   if (!error) {
     const $ = cheerio.load(body);
     contributions = $('g').find('rect').toString();
-    console.log(contributions);
+    contributions = contributions.split('</rect>');
+    const contributionsFiltered = [];
+    for (let i = 0; i < contributions.length; i += 1) {
+      if (contributions[i].search('data-count="0"') !== -1) {
+        contributionsFiltered.push(contributions[i].replace(/<.*?var\(/gm, ""));
+      }
+    }
+    console.log(contributions[0].replace(/<.*?var\(/gm, "").replace(/(")/gm, "").replace(/(\))/gm, "").replace(/(=)/gm, ""));
   } else {
     throw new Error(error);
   }

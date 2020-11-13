9672a76f0ede070daff7837daf6cf270c8bc5fe5
async-devil <async.devil@gmail.com>
2020-11-13 23:36:03 +0200

    Added few schemes

---
+++ src/colorSchemes.js
@@ -0,0 +1,18 @@
+const colorsSchemes = {
+  default: {
+    colorBg: '#ebedf0',
+    colorL1: '#9be9a8',
+    colorL2: '#40c463',
+    colorL3: '#30a14e',
+    colorL4: '#216e39',
+  },
+  sakura: {
+    colorBg: '#e5afc4',
+    colorL1: '#d782a3',
+    colorL2: '#cd648d',
+    colorL3: '#c44677',
+    colorL4: '#9d325c',
+  },
+};
+
+export { colorsSchemes as default };

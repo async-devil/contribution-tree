27346218a8e1b9501f1428f37ca8f4cf56d4215a
async-devil <async.devil@gmail.com>
2020-11-14 13:06:03 +0200

    Migrated to TypeScript

--- src/colorSchemes.ts
+++ src/colorSchemes.ts
@@ -1,5 +1,20 @@
-const colorsSchemes = {
+interface Colors {
+  colorErr: string,
+  colorBg: string,
+  colorL1: string,
+  colorL2: string,
+  colorL3: string,
+  colorL4: string,
+}
+
+interface ColorSchemes {
+  default: Colors,
+  sakura: Colors,
+}
+
+const colorsSchemes:ColorSchemes = {
   default: {
+    colorErr: '#000000', //^ Why not?
     colorBg: '#ebedf0',
     colorL1: '#9be9a8',
     colorL2: '#40c463',
@@ -7,6 +22,7 @@ const colorsSchemes = {
     colorL4: '#216e39',
   },
   sakura: {
+    colorErr: '#000000',
     colorBg: '#e5afc4',
     colorL1: '#d782a3',
     colorL2: '#cd648d',

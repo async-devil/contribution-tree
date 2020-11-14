42b71ffe8ebc502549ebcc6e9fe384959a4bad14
async-devil <async.devil@gmail.com>
2020-11-14 13:05:42 +0200

    Added colorSchemes support

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,42 @@
 /* eslint-disable no-console */
 const request = require('request');
 const cheerio = require('cheerio');
+import schemes from './colorSchemes'
 
 const user = 'async-devil';
 const url = `https://github.com/users/${user}/contributions`;
-let contributions:string[]|string;
+
+let contributions:string[];
 const contributionsFiltered:Object[] = [];
+
 type Data = {
   color: string,
   contributions: string,
   date: string,
 }
 
+function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+  return o[propertyName]; //& Thanks to official docs
+}
+
 const filter = (array:string) => {
   const colorMatch = (string:string) => {
-    switch (string) {
-      case '--color-calendar-graph-day-L1-bg':
-        return '#9be9a8';
-      case '--color-calendar-graph-day-L2-bg':
-        return '#40c463';
-      case '--color-calendar-graph-day-L3-bg':
-        return '#30a14e';
-      case '--color-calendar-graph-day-L4-bg':
-        return '#216e39';
-      default:
-        return '#ebedf0'; 
-    }
+    const scheme = "default";
+    const selectedScheme = getProperty(schemes, scheme)
+    
+    if (string.search('-L1-') !== -1) return selectedScheme.colorL1;
+    if (string.search('-L2-') !== -1) return selectedScheme.colorL2;
+    if (string.search('-L3-') !== -1) return selectedScheme.colorL3; //^ Don't know if this is a bad code or not
+    if (string.search('-L4-') !== -1) return selectedScheme.colorL4; //TODO: Make it more beautiful or smth
+    if (string.search('day-bg') !== -1) return selectedScheme.colorBg;
+
+    return selectedScheme.colorErr;
   };
-  console.log(array);
   const buffer = array
     .replace(/<.*?var\(/gm, '')
-    .replace(/(")/gm, '')
-    .replace(/(\))/gm, '')
+    .replace(/(")/gm, '') //^ This look gross, I dunno how to make it smaller
+    .replace(/(\))/gm, '') //TODO: Make it simpler
     .replace(/data.*?=/gm, '')
-    // eslint-disable-next-line comma-dangle
     .replace(/(>)/, '')
     .split(' ');
 
@@ -53,11 +56,13 @@ request(url, (error:any, response:any, body:any) => {
     contributions = $('g').find('rect').toString().split('</rect>');
 
     for (let i = 0; i < contributions.length; i += 1) {
-      contributionsFiltered.push(filter(contributions[i]));
+      contributionsFiltered.push(filter(contributions[i])); 
     }
 
-    console.log(contributionsFiltered);
+    console.log(contributionsFiltered[369]);
   } else {
     throw new Error(error);
   }
 });
+
+export {contributionsFiltered as default};

83b1ea05efd6d7e6949a6a1963322ffd5d8e5cf2
async-devil <async.devil@gmail.com>
2020-11-15 22:17:47 +0200

    Added integrations and made some cosmetic changes

---
+++ src/utils/main.ts
@@ -0,0 +1,73 @@
+const request = require('request');
+const cheerio = require('cheerio');
+import schemes from './colorSchemes';
+
+const importValues = {
+  scheme: 'default',
+  user: 'async-devil', //TODO: Import it from RestAPI (GraphQL)
+}
+
+const {scheme, user} = importValues;
+const url = `https://github.com/users/${user}/contributions`;
+
+interface Data {
+  color: string,
+  contributions: string,
+  date: string
+}
+
+const filter = (array:string) => {
+  const colorMatch = (string:string) => {
+    //* Grabbing theme from schemes
+    const selectedScheme = schemes[scheme];
+
+    //* Searching for L{num} if not found, null
+    let match:string[]|null = string.match(/L\d/gm); //
+
+    if (match === null) return selectedScheme.colorBg;
+    switch (match[0]) {
+        case 'L1':
+            return selectedScheme.colorL1;
+        case 'L2':
+            return selectedScheme.colorL2;
+        case 'L3':
+            return selectedScheme.colorL3;
+        case 'L4':
+            return selectedScheme.colorL4;
+        default:
+            return selectedScheme.colorErr;
+    }
+  };
+
+  //* Spliting data to array of css variable color, number of contributions and date
+  const buffer = array
+    .replace(/(<.*?var\()|(")|(\))|(data.*?=)|(>)/gm, '')
+    .split(' ');
+
+  const filtered:Data = {
+    color: colorMatch(buffer[0]),
+    contributions: buffer[1],
+    date: buffer[2],
+  };
+
+  return filtered;
+};
+
+const contributionsFiltered:Data[] = [];
+
+request(url, (error:any, response:any, body:any) => {
+  if (!error) {
+    const $ = cheerio.load(body);
+
+    //* Spliting output to an array by </rect>
+    let contributions:string[] = $('g').find('rect').toString().split('</rect>');
+
+    for (let i = 0; i < contributions.length; i += 1) {
+      contributionsFiltered.push(filter(contributions[i])); 
+    }
+  } else {
+    throw new Error(error);
+  }
+});
+
+export {contributionsFiltered as default};

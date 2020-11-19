4ec7a5b0589b1e7eed78e138075f62bd1be2bd3d
async-devil <async.devil@gmail.com>
2020-11-20 00:16:22 +0200

    Made test, todo: make it work

--- src/tests/genFillGraph.test.ts
+++ src/tests/genFillGraph.test.ts
@@ -1,1 +1,9 @@
-import Generator from '../utils/Generator';
+const xLines = require('../utils/Generator') 
+
+test('Generating 4x4 graph',() => { //TODO: fix the test
+    expect(xLines(4)).toBe(`<line x1="{{_ 0}}" y1="{{_ 0}}" x2="{{_ 210}}" y2="{{_ 0}}"/>
+<line x1="{{_ 0}}" y1="{{_ 25}}" x2="{{_ 210}}" y2="{{_ 25}}"/>
+<line x1="{{_ 0}}" y1="{{_ 50}}" x2="{{_ 210}}" y2="{{_ 50}}"/>
+<line x1="{{_ 0}}" y1="{{_ 75}}" x2="{{_ 210}}" y2="{{_ 75}}"/>
+<line x1="{{_ 0}}" y1="{{_ 100}}" x2="{{_ 210}}" y2="{{_ 100}}"/>`)
+})

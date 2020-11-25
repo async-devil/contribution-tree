01c6757985d695aaddef62329bcd3808e1e0d28a
async-devil <async.devil@gmail.com>
2020-11-25 23:55:09 +0200

    Updated themes

--- themes/SG-themes.ts
+++ themes/SG-themes.ts
@@ -1,75 +1,40 @@
 interface Data {
-  [key: string]: any;
+  [key: string]: Theme;
 }
 
 //! Please use colors in the same format
+interface Theme {
+  background: string;
+  line: string;
+  graph: string;
+  points: string;
+}
+
 const themes: Data = {
   default: {
-    colorLine: '#7a7a7a',
-    colorBackground: {
-      gradient: null,
-      singleColor: '#ffffff',
-    },
-    colorPoints: '#000000',
-    colorGraph: {
-      gradient: null,
-      singleColor: '#3aa25b',
-    },
+    line: '#7a7a7a',
+    background: '#ffffff',
+    points: '#000000',
+    graph: '#3aa25b',
   },
   greenGradient: {
-    colorLine: '#7a7a7a',
-    colorBackground: {
-      gradient: null,
-      singleColor: '#ffffff',
-    },
-    colorPoints: '#000000',
-    colorGraph: {
-      gradient: {
-        color1: '#8ed7a5', // 0%
-        color2: '#63c783', // 25%
-        color3: '#3aa25b', // 50%
-        color4: '#30854b', // 75%
-        color5: '#26683b', // 100%
-      },
-      singleColor: null,
-    },
+    line: '#7a7a7a',
+    background: '#ffffff',
+    points: '#000000',
+    graph: 'linear-gradient(0deg, #8ed7a5 0%, #63c783 25%, #3aa25b 50%, #30854b 75%, #26683b 100%)',
   },
   sakuraGradient: {
-    colorLine: '#7a7a7a',
-    colorBackground: {
-      gradient: null,
-      singleColor: '#ffffff',
-    },
-    colorPoints: '#000000',
-    colorGraph: {
-      gradient: {
-        color1: '#e5afc4', // 0%
-        color2: '#d782a3', // 25%
-        color3: '#cd648d', // 50%
-        color4: '#c44677', // 75%
-        color5: '#9d325c', // 100%
-      },
-      singleColor: null,
-    },
+    line: '#7a7a7a',
+    background: '#ffffff',
+    points: '#000000',
+    graph: 'linear-gradient(0deg, #e5afc4 0%, #d782a3 25%, #cd648d 50%, #c44677 75%, #9d325c 100%)',
   },
   blueGradient: {
-    colorLine: '#7a7a7a',
-    colorBackground: {
-      gradient: null,
-      singleColor: '#ffffff',
-    },
-    colorPoints: '#000000',
-    colorGraph: {
-      gradient: {
-        color1: '#66b5cb', // 0%
-        color2: '#3e9db8', // 25%
-        color3: '#34849b', // 50%
-        color4: '#2a6b7e', // 75%
-        color5: '#205261', // 100%
-      },
-      singleColor: null,
-    },
+    line: '#7a7a7a',
+    background: '#ffffff',
+    points: '#000000',
+    graph: 'linear-gradient(0deg, #66b5cb 0%, #3e9db8 25%, #34849b 50%, #2a6b7e 75%, #205261 100%)',
   },
 };
 
-export { themes as default };
+export { themes, Theme };

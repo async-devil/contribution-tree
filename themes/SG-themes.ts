aaddd66782047956ce3f2ab73381905eef5b5555
async-devil <async.devil@gmail.com>
2020-11-29 22:54:36 +0200

    Updated themes

--- themes/SG-themes.ts
+++ themes/SG-themes.ts
@@ -22,21 +22,21 @@ const themes: Data = {
     background: '#ffffff',
     points: '#000000',
     surface:
-      'linear-gradient(0deg, Gradient1, #8ed7a5 0%, #63c783 25%, #3aa25b 50%, #30854b 75%, #26683b 100%)',
+      'linear-gradient(0deg, id=Gradient1, #8ed7a5 0%, #63c783 25%, #3aa25b 50%, #30854b 75%, #26683b 100%)',
   },
   sakuraGradient: {
     line: '#7a7a7a',
     background: '#ffffff',
     points: '#000000',
     surface:
-      'linear-gradient(0deg, Gradient1, #e5afc4 0%, #d782a3 25%, #cd648d 50%, #c44677 75%, #9d325c 100%)',
+      'linear-gradient(0deg, id=Gradient1, #e5afc4 0%, #d782a3 25%, #cd648d 50%, #c44677 75%, #9d325c 100%)',
   },
   blueGradient: {
     line: '#7a7a7a',
     background: '#ffffff',
     points: '#000000',
     surface:
-      'linear-gradient(0deg, Gradient1, #66b5cb 0%, #3e9db8 25%, #34849b 50%, #2a6b7e 75%, #205261 100%)',
+      'linear-gradient(0deg, id=Gradient1, #66b5cb 0%, #3e9db8 25%, #34849b 50%, #2a6b7e 75%, #205261 100%)',
   },
 };
 

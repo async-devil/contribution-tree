e9abafdef1297d8c515e8a8e81867735b9a867f5
async-devil <async.devil@gmail.com>
2020-12-10 00:02:08 +0200

    Updated tests

--- tests/SG-tests/Styles.test.ts
+++ tests/SG-tests/Styles.test.ts
@@ -1,5 +1,6 @@
 import { Styles } from '../../api/StandartGraph/SG-Styles';
 
+/*------------------------------------------------------------------------------------------*/
 describe('MatchTheme method tests', () => {
   describe('Correct values', () => {
     test('Checking if method founds sakuraGradient theme', () => {
@@ -36,18 +37,45 @@ describe('MatchTheme method tests', () => {
     });
   });
 });
+/*------------------------------------------------------------------------------------------*/
 
+/*------------------------------------------------------------------------------------------*/
 describe('ColorOrGradientParcer method tests', () => {
   describe('Correct values', () => {
     test('Checking if method returns valid 3 digital hex', () => {
       const Style = new Styles('');
-      expect(Style.colorOrGradientParcer('#3a3')).toBe('#3a3');
+      expect(Style.colorOrGradientParcer('#3a3')).toStrictEqual({
+        result: {
+          CSS: '#3a3',
+        },
+      });
     });
     test('Checking if method returns valid 6 digital hex', () => {
       const Style = new Styles('');
-      expect(Style.colorOrGradientParcer('#ab4356')).toBe('#ab4356');
+      expect(Style.colorOrGradientParcer('#ab4356')).toStrictEqual({
+        result: {
+          CSS: '#ab4356',
+        },
+      });
+    });
+    test('Checking if method returns valid gradient info', () => {
+      const Style = new Styles('');
+      expect(
+        Style.colorOrGradientParcer(
+          'linear-gradient(0deg, id=Gradient1, #e5afc4 0%, #d782a3 25%, #cd648d 50%, #c44677 75%, #9d325c 100%)',
+        ),
+      ).toStrictEqual({
+        result: {
+          CSS:
+            'linear-gradient(0deg, #e5afc4 0%, #d782a3 25%, #cd648d 50%, #c44677 75%, #9d325c 100%)',
+          SVG: {
+            CSS: '',
+          },
+        },
+      });
     });
   });
 
   describe('Incorrect values', () => {});
 });
+/*------------------------------------------------------------------------------------------*/

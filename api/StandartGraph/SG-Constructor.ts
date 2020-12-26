e2209d4384630627a64b562d0c10a0b5ddc05718
async-devil <async.devil@gmail.com>
2020-12-27 01:11:14 +0200

    Updated Constructor

--- api/StandartGraph/SG-Constructor.ts
+++ api/StandartGraph/SG-Constructor.ts
@@ -1,22 +1,21 @@
-import { Lines, Info } from './SG-Lines';
+import { Data, Info } from './SG-Data';
+import { Lines } from './SG-Lines';
 import { Styles } from './SG-Styles';
 
-class Constructor extends Lines {
+class Constructor extends Data {
   constructor(data: Info) {
     super(data);
   }
 
-  public generate() {
-    let buffer: string[] = [];
-    // prettier-ignore
-    buffer.push(
-    this.svgStart(), 
-    this.initX(), 
-    this.initY(), 
-    this.svgEnd);
+  public construct() {
+    const _Lines = new Lines(this.data);
+    const _Styles = new Styles(this.data.choice);
+
+    const buffer: string[] = [];
+    buffer.push(this.svgStart(), _Lines.generate(), this.svgEnd);
 
     return buffer.join('\n');
   }
 }
 
-export { Constructor, Info };
+export { Constructor };

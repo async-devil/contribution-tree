ec6dc209faba25b18d836292989fb16160982b6d
async-devil <async.devil@gmail.com>
2021-01-15 00:05:05 +0200

    Deleted usless comments

--- themes/SG-themes.ts
+++ themes/SG-themes.ts
@@ -12,11 +12,6 @@ type Theme = {
 };
 
 const themes: Data = {
-  /*
-  ! Before changing please update to your purpuses:
-  & SG-Styles.ts on 79 line
-  & Styles.test.ts on 88 line
-  */
   default: {
     line: '#7a7a7a',
     background: '#ffffff',

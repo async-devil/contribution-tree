c9b48efadd8ec84a26eafe2988cd50b3541ef5d0
async-devil <async.devil@gmail.com>
2021-01-16 23:58:16 +0200

    Added few classes and updated before ones

--- api/StandartGraph/SG-Styles.ts
+++ api/StandartGraph/SG-Styles.ts
@@ -1,31 +1,97 @@
 import { themes, Theme } from '../../themes/SG-themes';
 import GTSF from './SG-GradientToSVG';
 
+/**
+ * Output of {@link colorOrGradient} method
+ * @alias colorOrGradientOutput
+ */
+type colorOrGradientOutput = {
+  /** Type of output */
+  type: string;
+  result: {
+    /** CSS code or color*/
+    css?: string;
+    /** CSV code */
+    svg?: {
+      /** CSS code from SVG */
+      css: string;
+      /** HTML code from SVG */
+      html: string;
+    };
+  };
+};
+
+/**
+ * Type which is describing element and its properties
+ * @alias Elements
+ */
+type Elements = {
+  /** Element name */
+  [key: string]: Property;
+};
+
+/**
+ * Component of {@link Elements}
+ * @alias Property
+ */
+type Property = {
+  /** HTML element which needs to be stylished */
+  element: string;
+  /** CSS property of element */
+  property: string;
+  /** Is HTML required (like in SVG) */
+  html: boolean;
+};
+/*------------------------------------------------------------------------------------------*/
+
+/** Data containing class */
 class Data {
-  public readonly dev: boolean;
   public readonly selectedTheme: string;
 
   /**
    * @param selectedTheme Theme which is need to be parced
-   * @param dev Dev mode enabling
    */
-  constructor(selectedTheme: string, dev?: boolean) {
+  constructor(selectedTheme: string) {
     this.selectedTheme = selectedTheme;
-    this.dev = dev || false;
   }
+}
 
-  protected devLog(message?: any) {
+/*------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------*/
+
+/** Utilities functionality class */
+class Utilities {
+  /** @param dev Dev logs enabling */
+  public dev: boolean = false;
+
+  /**
+   * Setter which can enable and disable devMode
+   * @param {boolean} enable On or off choice
+   */
+  public set devMode(enable: boolean) {
+    this.dev = enable;
+  }
+  /**
+   * Console log which is called only if dev param is true
+   * @param {any} message Message which is need to be logged
+   */
+  public devLog(message?: any) {
     if (this.dev === true) {
       console.log(message);
     }
   }
 }
 
+/*------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------*/
+
 /** Theme checking functionality */
 class CheckTheme extends Data {
   /**
    * Matches theme by name, if theme not found returns default one
-   * @returns {Theme} {@link Theme}
+   * @returns {Theme}
    */
   public matchTheme(): Theme {
     return themes[this.selectedTheme] || themes.default;
@@ -40,45 +106,53 @@ class CheckTheme extends Data {
   }
 }
 
-class colorOrGradient extends Data {
-  private data: string = '';
+/*------------------------------------------------------------------------------------------*/
 
-  protected set toCheck(data: string) {
-    this.data = data;
+/*------------------------------------------------------------------------------------------*/
+
+/** Class which contains method to parse color or gradient into a CSS or SVG property */
+class ColorOrGradient {
+  private input: string;
+
+  /** @param {string} input Hex color or CSS-like gradient */
+  constructor(input: string) {
+    this.input = input;
   }
 
-  private get getResult() {
-    return this.parse(this.data);
+  /**
+   * Getter which returns parsed input
+   * @returns {colorOrGradientOutput|Error} Output object or error
+   */
+  public get getResult() {
+    return this.parse();
   }
 
-  private parse(data: string) {
+  private parse() {
+    const util = new Utilities();
+    util.devMode = false; //! DevMode enabling
+
     //^ If data is hex, than returns data as CSS
-    if (data.search(/(^#[0-9a-fA-F]{3}$)|(^#[0-9a-fA-F]{6}$)/gm) !== -1) {
-      const output: any = {
+    if (this.input.search(/(^#[0-9a-fA-F]{3}$)|(^#[0-9a-fA-F]{6}$)/gm) !== -1) {
+      const output: colorOrGradientOutput = {
+        type: 'color',
         result: {
-          CSS: data,
+          css: this.input,
         },
       };
       return output;
     }
 
-    const gtsf = new GTSF(data);
+    util.devLog(this.input);
+    const gtsf = new GTSF(this.input);
     const SVG = gtsf.construct();
 
-    //^If data contains invalid info than returns error
-    if (SVG.error !== undefined || SVG.result === undefined) {
-      const output: any = {
-        error: SVG.error,
-      };
-      return output;
-    }
-
-    const output: any = {
+    const output: colorOrGradientOutput = {
+      type: 'gradient',
       result: {
-        CSS: gtsf.parcedGradientInfoToCSS(gtsf.regexCut(data)),
-        SVG: {
-          CSS: SVG.result.css,
-          HTML: SVG.result.html,
+        css: gtsf.parsedGradientInfoToCSS(gtsf.regexCut(this.input)),
+        svg: {
+          css: SVG.css,
+          html: SVG.html,
         },
       },
     };
@@ -87,13 +161,95 @@ class colorOrGradient extends Data {
   }
 }
 
+/*------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------*/
+
+/** Class which contains methods to set CSS properties to graph element */
+class PropertyDefiningKey extends Data {
+  protected element: string;
+
+  /**
+   * @param {string} selectedTheme Theme which is need to be parced
+   * @param {string} element Graph element
+   */
+  constructor(selectedTheme: string, element: string) {
+    super(selectedTheme);
+    this.element = element;
+  }
+
+  /** Getter which returns supported elements*/
+  public get getElements(): Elements {
+    return this.elements;
+  }
+
+  protected elements: Elements = {
+    line: {
+      element: '.grid',
+      property: 'stroke',
+      html: false,
+    },
+    background: {
+      element: 'svg',
+      property: 'background',
+      html: false,
+    },
+    points: {
+      element: '.points',
+      property: 'fill',
+      html: false,
+    },
+    surface: {
+      element: '.surfaces',
+      property: 'fill',
+      html: true,
+    },
+  };
+
+  /**
+   * Getter which returns CSS properties of specific graph element
+   * @returns {Property|Error} Property object or error
+   */
+  public get getResult(): Property {
+    return this.parse();
+  }
+
+  private parse(): Property {
+    const util = new Utilities();
+    util.devMode = false; //! DevMode enabling
+
+    const CT = new CheckTheme(this.selectedTheme);
+    //^ Getting type keys via its child
+    const matchThemeKeys = Object.keys(CT.matchTheme());
+
+    //^ If keys don`t contain property, throws error
+    if (!matchThemeKeys.includes(this.element)) {
+      util.devLog(this.element);
+      throw new Error('Invalid property');
+    }
+
+    //^ Declaring parametrs of theme property
+    const elementsKeys = Object.keys(this.elements);
+    for (let i = 0; i < elementsKeys.length; i += 1) {
+      if (elementsKeys[i] === this.element) {
+        return this.elements[elementsKeys[i]];
+      }
+    }
+
+    util.devLog(this.elements);
+    throw new Error('Parametrs not found');
+  }
+}
+
+/*------------------------------------------------------------------------------------------*/
+
 class Styles extends Data {
-  constructor(selectedTheme: string, dev?: boolean) {
-    super(selectedTheme, dev);
+  constructor(selectedTheme: string) {
+    super(selectedTheme);
   }
 
   protected transform() {}
 }
 
 export { Styles as default };
-export { CheckTheme, colorOrGradient };
+export { CheckTheme, ColorOrGradient, PropertyDefiningKey };
--- tests/SG-tests/Styles.test.ts
+++ tests/SG-tests/Styles.test.ts
@@ -1,31 +1,108 @@
-import { CheckTheme } from '../../api/StandartGraph/SG-Styles';
+import {
+  CheckTheme,
+  ColorOrGradient,
+  PropertyDefiningKey,
+} from '../../api/StandartGraph/SG-Styles';
 import { themes } from '../../themes/SG-themes';
 
 /*------------------------------------------------------------------------------------------*/
 
 describe('CheckTheme tests', () => {
   test('Checking if matchTheme works correct', () => {
-    const _CheckTheme = new CheckTheme('default', true);
+    const _CheckTheme = new CheckTheme('default');
     const test = _CheckTheme.matchTheme();
     expect(test).toBe(themes.default);
   });
 
   test('Checking if matchTheme works correct with invalid theme name', () => {
-    const _CheckTheme = new CheckTheme('__neverUseThisNameInProduction', true);
+    const _CheckTheme = new CheckTheme('__neverUseThisInProduction');
     const test = _CheckTheme.matchTheme();
     expect(test).toBe(themes.default);
   });
 
   test('Checking if isValidTheme works correct', () => {
-    const _CheckTheme = new CheckTheme('default', true);
+    const _CheckTheme = new CheckTheme('default');
     const test = _CheckTheme.isValidTheme();
     expect(test).toBe(true);
   });
 
   test('Checking if isValidTheme works correct with invalid theme name', () => {
-    const _CheckTheme = new CheckTheme('__neverUseThisNameInProduction', true);
+    const _CheckTheme = new CheckTheme('__neverUseThisInProduction');
     const test = _CheckTheme.isValidTheme();
     expect(test).toBe(false);
+  });
+});
+
+/*------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------*/
+
+describe('colorOrGradient tests', () => {
+  describe('Correct values', () => {
+    test('Checking if getResutlt returns color output', () => {
+      const _ColorOrGradient = new ColorOrGradient('#123456');
+      const test = _ColorOrGradient.getResult;
+      expect(test).toStrictEqual({
+        type: 'color',
+        result: {
+          css: '#123456',
+        },
+      });
+    });
+
+    test('Checking if getResutlt returns color output', () => {
+      const _ColorOrGradient = new ColorOrGradient(
+        'linear-gradient(0deg, id="Gradient1", #8ed7a5 0%, #63c783 25%, #3aa25b 50%, #30854b 75%, #26683b 100%)',
+      );
+      const test = _ColorOrGradient.getResult;
+      expect(test).toStrictEqual({
+        type: 'gradient',
+        result: {
+          css:
+            'linear-gradient(0deg, #8ed7a5 0%, #63c783 25%, #3aa25b 50%, #30854b 75%, #26683b 100%)',
+          svg: {
+            css: 'fill: url(#Gradient1)',
+            html: expect.any(String),
+          },
+        },
+      });
+    });
+  });
+
+  describe('Incorrect values', () => {
+    test('Checking if getResutlt throws error if input data is invalid', () => {
+      const _ColorOrGradient = new ColorOrGradient('__neverUseThisInProduction');
+      try {
+        const test = _ColorOrGradient.getResult;
+      } catch (err) {
+        expect(err.message).toStrictEqual('Invalid gradient data');
+      }
+    });
+  });
+});
+
+/*------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------*/
+
+describe('PropertyDefiningKey tests', () => {
+  describe('Correct values', () => {
+    test('Checking if chooseColorDefiningWord method returs valid info', () => {
+      const _PropertyDefiningKey = new PropertyDefiningKey('default', 'line');
+      const test = _PropertyDefiningKey.getResult;
+      expect(test).toBe(_PropertyDefiningKey.getElements.line);
+    });
+  });
+
+  describe('Incorrect values', () => {
+    test('Checking if getResutlt throws error if input data is invalid', () => {
+      const _PropertyDefiningKey = new PropertyDefiningKey('default', '__neverUseThisInProduction');
+      try {
+        const test = _PropertyDefiningKey.getResult;
+      } catch (err) {
+        expect(err.message).toStrictEqual('Invalid property');
+      }
+    });
   });
 });
 

addcf57a018eb619d37ac16c2c488e0ad3a3962d
async-devil <async.devil@gmail.com>
2021-01-20 23:54:12 +0200

    Finished StandartGraph

--- api/StandartGraph/StandartGraph.ts
+++ api/StandartGraph/StandartGraph.ts
@@ -1,5 +1,38 @@
 import { Constructor } from './SG-Constructor';
+import { Data, Info } from './SG-Data';
 
-class StandartGraph {}
+class StandartGraph extends Constructor {
+  /**
+   * @param data Construction info about graph
+   * @param choice Graph theme name
+   * @param pointsPercentage Percentage of each point location number of values must be number of rows + 1
+   */
+  constructor(
+    protected data: Info,
+    protected choice: string,
+    protected pointsPercentage: Array<number>,
+  ) {
+    super(data, choice, pointsPercentage);
+  }
+
+  /**
+   * Getter which return SVG graph
+   * @returns {string} SVG graph
+   */
+  public get getGradient(): string {
+    return this.check();
+  }
+
+  private check(): string {
+    let result: string;
+    try {
+      result = this.construct();
+    } catch (err) {
+      result = err.message;
+    }
+
+    return result;
+  }
+}
 
 export { StandartGraph };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /*------------------------------------*/
 require('dotenv').config();
 const express = require('express');
+import { StandartGraph } from '../api/StandartGraph/StandartGraph';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,24 @@ const port = process.env.PORT || 3000;
 app.get('', (req: any, res: any) => {
   res.setHeader('Cache-Control', `public, max-age=300`);
   res.setHeader('Content-Type', 'image/svg+xml');
+  const SG = new StandartGraph(
+    {
+      startPoints: {
+        x: 3,
+        y: 3,
+      },
+      insideWidth: 200,
+      insideHeight: 100,
+      outsideWidth: 10,
+      outsideHeight: 10,
+      rows: 10,
+      columns: 10,
+      factor: 5,
+    },
+    'blueGradient',
+    [0, 25, 30, 28, 44, 40, 42, 38, 20, 22, 10, 44].sort(),
+  );
+  res.send(`${SG.getGradient}`);
 });
 /*------------------------------------*/
 
